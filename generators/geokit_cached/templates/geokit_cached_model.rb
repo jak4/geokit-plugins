8e3bc597a8e61d0162ed53ddbd3d67ed6497cf85
jak4 <jak4@gmx.net>
2009-12-18 12:49:32 +0100

    removed some legacy stuff from my model

--- generators/geokit_cached/templates/geokit_cached_model.rb
+++ generators/geokit_cached/templates/geokit_cached_model.rb
@@ -1,13 +1,3 @@
 class <%= class_name %> < ActiveRecord::Base
   include GeokitPlugins::GeokitCached
-  # config options for to_xml
-  XML_OPTIONS = {:include => [], :except => [] + self.protected_attributes().to_a, :methods => [:geocoding_errors]}
-  
-  alias_method :ar_to_xml, :to_xml
-  def to_xml(options = {})
-    options[:except] = (options[:except] ? Array(options[:except]) + XML_OPTIONS[:except] : XML_OPTIONS[:except])
-    options[:include] = (options[:include] ? Array(options[:include]) + XML_OPTIONS[:include] : XML_OPTIONS[:include])
-    options[:methods] = (options[:methods] ? Array(options[:methods]) + XML_OPTIONS[:methods] : XML_OPTIONS[:methods])
-    ar_to_xml(options)
-  end
 end
